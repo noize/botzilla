b5dc0982f9aa2b1b43d31b53269a5f54098a517a
noize <chaoticnoizeoffspring@gmail.com>
2017-02-02 01:09:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import socket
+import socket, time
 
 """ bot_config class used to package information to give to bot so it can do its thing """
 class bot_config:
@@ -12,21 +12,23 @@ class irc_bot:
         self.port = port
         self.botconfig = botconfig
 
-    def connect(self): 
+    def connect(self):
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.connect((self.server, self.port))
-        self.register()
-        while(True): # for debug purpouses
-            print self.connection.recv(1024)
 
     def register(self):
         self.connection.send("USER {} {} {} : test bot lel\n\n".format(self.botconfig.nick, self.botconfig.nick, self.botconfig.nick))
         self.connection.send("NICK {}\n\n".format(self.botconfig.nick))
         self.connection.send("JOIN {}\n\n".format(self.channel))
 
+    def say(self, message):
+        self.connection.send("PRIVMSG {} :{}\n\n".format(self.channel, message))
+
 if (__name__ == "__main__"):
     conf = bot_config
     conf.nick = "testBotzillaBot"
-    bot = irc_bot("irc.freenode.net", 6667, "#tempBotzillaChat", conf)
+    bot = irc_bot("irc.freenode.net", 6667, "#BotzillaBotTesting", conf)
     bot.connect()
     bot.register()
+    bot.say("Look ma! I can talk now")
+    while(True):pass #should be replaced later with more productive loop
